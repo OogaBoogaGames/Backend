7278c999326bf5af8f6fa3ed8116291cfbbee71b
endercass <46408156+Endercass@users.noreply.github.com>
2023-09-12 21:58:00 -0400

    Functioning bundle server

--- src/object_storage/integrated/bundles.rs
+++ src/object_storage/integrated/bundles.rs
@@ -1,11 +1,15 @@
 use std::path::PathBuf;
 
 use axum::{
+    body::StreamBody,
     extract::{Path, State},
     http::StatusCode,
+    response::IntoResponse,
 };
+use scorched::{log_this, LogData, LogImportance};
+use tokio_util::io::ReaderStream;
 
-pub async fn get_raw(state: State<PathBuf>, Path(package): Path<String>) -> (StatusCode, Vec<u8>) {
+pub async fn get_raw(state: State<PathBuf>, Path(package): Path<String>) -> impl IntoResponse {
     match state.canonicalize() {
         Ok(base_path) => {
             let mut full_path = base_path.clone();
@@ -13,22 +17,46 @@ pub async fn get_raw(state: State<PathBuf>, Path(package): Path<String>) -> (Sta
 
             match full_path.canonicalize() {
                 Ok(full_path) => {
-                    println!("{}", full_path.display());
-                    // "hi" in utf-8
-                    (StatusCode::OK, vec![104, 105, 10])
+                    if full_path.starts_with(base_path) {
+                        log_this(LogData {
+                            importance: LogImportance::Info,
+                            message: format!("Serving bundle from path: {}", full_path.display()),
+                        })
+                        .await;
+                        let file = match tokio::fs::File::open(full_path).await {
+                            Ok(file) => file,
+                            Err(err) => {
+                                return Err((
+                                    StatusCode::NOT_FOUND,
+                                    "Error fetching resource.".to_string(),
+                                ));
+                            }
+                        };
+                        let stream = ReaderStream::new(file);
+                        let body = StreamBody::new(stream);
+
+                        return Ok((StatusCode::OK, body));
+                    }
+                    Err((StatusCode::FORBIDDEN, "Forbidden directory.".to_string()))
                 }
                 Err(e) => {
                     if e.raw_os_error() == Some(2) {
-                        return (StatusCode::NOT_FOUND, vec![]);
+                        return Err((StatusCode::NOT_FOUND, "Unknown resource.".to_string()));
                     }
                     println!("Error canonicalizing full path for asset serving: {}", e);
-                    (StatusCode::INTERNAL_SERVER_ERROR, vec![])
+                    Err((
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        "Error fetching resource.".to_string(),
+                    ))
                 }
             }
         }
         Err(e) => {
             println!("Error canonicalizing base path for asset serving: {}", e);
-            (StatusCode::INTERNAL_SERVER_ERROR, vec![])
+            Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Error fetching resource.".to_string(),
+            ))
         }
     }
 }
